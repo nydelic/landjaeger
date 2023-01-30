bfb8f24712d2def919317f58d6ec3230586e9267
Josias <josias.ribi@gmail.com>
2023-01-30 16:06:56 +0100

    fix: try w/ next.config redirect to not confuse google

--- next.config.js
+++ next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/landjaeger-mit-brot', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
 };
 
 module.exports = nextConfig;
